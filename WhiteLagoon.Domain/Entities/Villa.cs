dd28796cdaf95a53ad02b5191c62658ec70672ac
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-23 21:33:40 +0800

    SeedVillaToDb

--- WhiteLagoon.Domain/Entities/Villa.cs
+++ WhiteLagoon.Domain/Entities/Villa.cs
@@ -12,7 +12,7 @@ namespace WhiteLagoon.Domain.Entities
         public double Price { get; set; }
         public int Sqft { get; set; }
         public int Occupancy { get; set; }
-        public string? ImageUrl {  get; set; }  
+        public string? ImageUrl { get; set; }
         public DateTime? CreatedDate { get; set; }
         public DateTime? UpdatedDate { get; set; }
     }
