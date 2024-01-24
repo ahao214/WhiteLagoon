45ea88762f296e4deba9818a4d317721286014d3
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-24 17:09:54 +0800

    upload image

--- WhiteLagoon.Domain/Entities/Villa.cs
+++ WhiteLagoon.Domain/Entities/Villa.cs
@@ -1,5 +1,7 @@
 ﻿
+using Microsoft.AspNetCore.Http;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WhiteLagoon.Domain.Entities
 {
@@ -18,6 +20,8 @@ namespace WhiteLagoon.Domain.Entities
         public int Sqft { get; set; }
         [Range(1, 10)]
         public int Occupancy { get; set; }
+        [NotMapped]
+        public IFormFile? Image { get; set; }
         [Display(Name = "Image Url")]
         public string? ImageUrl { get; set; }
         public DateTime? CreatedDate { get; set; }
