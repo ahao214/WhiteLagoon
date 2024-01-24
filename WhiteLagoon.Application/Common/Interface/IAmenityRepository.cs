3e8aa13580e91a7801b4e2b14057bccd11c29539
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-24 19:01:39 +0800

    Amenity Interface

---
+++ WhiteLagoon.Application/Common/Interface/IAmenityRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq.Expressions;
+using WhiteLagoon.Domain.Entities;
+
+namespace WhiteLagoon.Application.Common.Interface
+{
+    public interface IAmenityRepository : IBaseRepository<Amenity>
+    {
+        void Update(Amenity entity);        
+    }
+}
---
+++ WhiteLagoon.Infrastructure/Repository/AmenityRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using WhiteLagoon.Application.Common.Interface;
+using WhiteLagoon.Domain.Entities;
+using WhiteLagoon.Infrastructure.Data;
+
+namespace WhiteLagoon.Infrastructure.Repository
+{
+    public class AmenityRepository : BaseRepository<Amenity>, IAmenityRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public AmenityRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Amenity entity)
+        {
+            _db.Amenities.Update(entity);
+        }
+    }
+}
