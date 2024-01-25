7cfbcfe13f93baa5cecc9bd5ee109e184e6a2217
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-25 16:33:29 +0800

    returnUrl

--- WhiteLagoon.Web/Controllers/AccountController.cs
+++ WhiteLagoon.Web/Controllers/AccountController.cs
@@ -45,8 +45,10 @@ namespace WhiteLagoon.Web.Controllers
             return View();
         }
 
-        public IActionResult Register()
+        public IActionResult Register(string returnUrl = null)
         {
+            returnUrl ??= Url.Content("~/");
+
             if (!_roleManager.RoleExistsAsync(SD.Role_Admin).GetAwaiter().GetResult())
             {
                 _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).Wait();
@@ -59,7 +61,8 @@ namespace WhiteLagoon.Web.Controllers
                 {
                     Text = x.Name,
                     Value = x.Name
-                })
+                }),
+                RedirectUrl = returnUrl
             };
 
             return View(registerVM);
