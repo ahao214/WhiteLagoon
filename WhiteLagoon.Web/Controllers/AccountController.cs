ea75a6b56971d6b28e7b9daf452f3fc6f7f71704
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-25 15:21:50 +0800

    Add roles in the table

--- WhiteLagoon.Web/Controllers/AccountController.cs
+++ WhiteLagoon.Web/Controllers/AccountController.cs
@@ -34,6 +34,11 @@ namespace WhiteLagoon.Web.Controllers
 
         public IActionResult Register()
         {
+            if (!_roleManager.RoleExistsAsync("Admin").GetAwaiter().GetResult())
+            {
+                _roleManager.CreateAsync(new IdentityRole("Admin")).Wait();
+                _roleManager.CreateAsync(new IdentityRole("Customer")).Wait();
+            }
             return View();
         }
     }
