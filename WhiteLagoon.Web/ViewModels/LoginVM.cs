e82c9ca10f974442a8e82b457388eb52f27b196d
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-25 14:52:10 +0800

    RegisterVM LoginVM

---
+++ WhiteLagoon.Web/ViewModels/LoginVM.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WhiteLagoon.Web.ViewModels
+{
+    /// <summary>
+    /// 登录视图模型
+    /// </summary>
+    public class LoginVM
+    {
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+        public string? RedirectUrl { get; set; }
+    }
+
+}
