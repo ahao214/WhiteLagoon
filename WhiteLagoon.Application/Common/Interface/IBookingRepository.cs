f9f59e374ef86eff7f84cb4cff1beca0690a445c
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-26 16:15:39 +0800

    BookingRepository

---
+++ WhiteLagoon.Application/Common/Interface/IBookingRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq.Expressions;
+using WhiteLagoon.Domain.Entities;
+
+namespace WhiteLagoon.Application.Common.Interface
+{
+    public interface IBookingRepository : IBaseRepository<Booking>
+    {
+        void Update(Booking entity);        
+    }
+}
---
+++ WhiteLagoon.Infrastructure/Repository/BookingRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using WhiteLagoon.Application.Common.Interface;
+using WhiteLagoon.Domain.Entities;
+using WhiteLagoon.Infrastructure.Data;
+
+namespace WhiteLagoon.Infrastructure.Repository
+{
+    public class BookingRepository : BaseRepository<Booking>, IBookingRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public BookingRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Booking entity)
+        {
+            _db.Bookings.Update(entity);
+        }
+    }
+}
