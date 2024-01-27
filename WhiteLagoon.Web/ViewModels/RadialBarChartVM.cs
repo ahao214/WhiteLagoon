3e7aaaf6c9993afe80f325dce39dd4077fe36015
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-27 13:24:53 +0800

    RadialBarChartVM

---
+++ WhiteLagoon.Web/ViewModels/RadialBarChartVM.cs
@@ -0,0 +1,11 @@
+﻿namespace WhiteLagoon.Web.ViewModels
+{
+    public class RadialBarChartVM
+    {
+        public decimal TotalCount { get; set; }
+        public decimal IncreasdDecreaseAmount { get; set; }
+        public bool HasRatioIncreased { get; set; }
+        public decimal[] Series { get; set; }
+
+    }
+}
