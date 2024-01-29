7e5eb2f30adec051f149aa3e932316184a5302de
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-29 17:46:07 +0800

    GetRadialCartDataModel

--- WhiteLagoon.Application/Common/Utility/SD.cs
+++ WhiteLagoon.Application/Common/Utility/SD.cs
@@ -53,25 +53,25 @@ namespace WhiteLagoon.Application.Common.Utility
             return finalAvailableRoomForAllNights;
         }
 
-        //public static RadialBarChartDto GetRadialCartDataModel(int totalCount, double currentMonthCount, double prevMonthCount)
-        //{
-        //    RadialBarChartDto RadialBarChartDto = new();
+        public static RadialBarChartDto GetRadialCartDataModel(int totalCount, double currentMonthCount, double prevMonthCount)
+        {
+            RadialBarChartDto RadialBarChartDto = new();
 
 
-        //    int increaseDecreaseRatio = 100;
+            int increaseDecreaseRatio = 100;
 
-        //    if (prevMonthCount != 0)
-        //    {
-        //        increaseDecreaseRatio = Convert.ToInt32((currentMonthCount - prevMonthCount) / prevMonthCount * 100);
-        //    }
+            if (prevMonthCount != 0)
+            {
+                increaseDecreaseRatio = Convert.ToInt32((currentMonthCount - prevMonthCount) / prevMonthCount * 100);
+            }
 
-        //    RadialBarChartDto.TotalCount = totalCount;
-        //    RadialBarChartDto.CountInCurrentMonth = Convert.ToInt32(currentMonthCount);
-        //    RadialBarChartDto.HasRatioIncreased = currentMonthCount > prevMonthCount;
-        //    RadialBarChartDto.Series = new int[] { increaseDecreaseRatio };
+            RadialBarChartDto.TotalCount = totalCount;
+            RadialBarChartDto.CountInCurrentMonth = Convert.ToInt32(currentMonthCount);
+            RadialBarChartDto.HasRatioIncreased = currentMonthCount > prevMonthCount;
+            RadialBarChartDto.Series = new int[] { increaseDecreaseRatio };
 
-        //    return RadialBarChartDto;
-        //}
+            return RadialBarChartDto;
+        }
 
 
     }
