bf426d01ca1f0123fa539b53ab32ee50f1b9f8a3
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-29 19:00:45 +0800

    Initialize

---
+++ WhiteLagoon.Infrastructure/Data/DbInitializer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WhiteLagoon.Application.Services.Interface;
+
+namespace WhiteLagoon.Infrastructure.Data
+{
+    public class DbInitializer : IDbInitializer
+    {
+        private readonly ApplicationDbContext _db;
+
+        public DbInitializer(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public void Initialize()
+        {
+            try
+            {
+                if(_db.Database.GetPendingMigrations().Count()> 0)
+                {
+                    _db.Database.Migrate();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+    }
+}
