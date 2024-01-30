f779f6f3f832cda032704cc7d61f4146ad571d4d
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-30 13:56:46 +0800

    Initializer

--- WhiteLagoon.Infrastructure/Data/DbInitializer.cs
+++ WhiteLagoon.Infrastructure/Data/DbInitializer.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using WhiteLagoon.Application.Common.Utility;
 using WhiteLagoon.Application.Services.Interface;
+using WhiteLagoon.Domain.Entities;
 
 namespace WhiteLagoon.Infrastructure.Data
 {
@@ -16,14 +17,14 @@ namespace WhiteLagoon.Infrastructure.Data
         private readonly Microsoft.AspNetCore.Identity.RoleManager<IdentityRole> _roleManager;
         private readonly UserManager<IdentityUser> _userManager;
 
-        public DbInitializer(ApplicationDbContext db,RoleManager<IdentityRole> roleManager,UserManager<IdentityUser> userManager)
+        public DbInitializer(ApplicationDbContext db, RoleManager<IdentityRole> roleManager, UserManager<IdentityUser> userManager)
         {
             _db = db;
             _roleManager = roleManager;
             _userManager = userManager;
         }
 
-        
+
 
         public void Initializer()
         {
@@ -33,11 +34,25 @@ namespace WhiteLagoon.Infrastructure.Data
                 {
                     _db.Database.Migrate();
                 }
-                if(_roleManager .RoleExistsAsync(SD.Role_Admin ).GetAwaiter().GetResult()) {
+                if (_roleManager.RoleExistsAsync(SD.Role_Admin).GetAwaiter().GetResult())
+                {
                     _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).Wait();
                     _roleManager.CreateAsync(new IdentityRole(SD.Role_Customer)).Wait();
 
+                    _userManager.CreateAsync(new ApplicationUser
+                    {
+                        UserName = "admin@gmail.com",
+                        Email = "admin.gmail.com",
+                        Name = "joker",
+                        NormalizedUserName = "ADMIN@GMAIL.COM",
+                        NormalizedEmail = "ADMIN@GMAIL.COM",
+                        PhoneNumber = "110",
+                    }, "Admin123*");
+                    ApplicationUser user = _db.ApplicationUsers.FirstOrDefault(u => u.Email == "admin@gmail.com");
+                    _userManager.AddToRoleAsync(user, SD.Role_Admin).GetAwaiter().GetResult();
                 }
+
+               
             }
             catch (Exception ex)
             {
